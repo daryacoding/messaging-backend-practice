5c19577843fd79868e88d9315df26ddb4627a1bf
daryacoding <abdullahzadeh.darya@gmail.com>
2023-02-18 00:11:01 -0500

    sidebar created

--- client/src/components/ChannelListContainer.js
+++ client/src/components/ChannelListContainer.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import { ChannelList, useChatContext } from 'stream-react-chat'
+import { ChannelList, useChatContext } from 'stream-chat-react'
 import Cookies from 'universal-cookie'
 
-import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './'
+import { ChannelSearch, TeamChannelList, TeamChannelPreview } from '.'
 import HospitalIcon from '../assets/hospital.png'
+import LogoutIcon from '../assets/logout.png'
 
 const SideBar = () => (
     <div className='channel-list__sidebar'>
@@ -11,13 +12,20 @@ const SideBar = () => (
             <div className='icon1__inner'>
                 <img src={HospitalIcon} alt='Hospital' width="30" />
             </div>
+            <div className='channel-list__sidebar__icon2'>
+            <div className='icon2__inner'>
+                <img src={LogoutIcon} alt='Logout' width="30" />
+            </div>
+        </div>
         </div>
     </div>
 )
 
 function ChannelListContainer() {
     return (
-        <div>ChannelListContainer</div>
+        <>
+            <SideBar />
+        </>
     )
 }
 
